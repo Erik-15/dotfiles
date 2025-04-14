a8cbc01d6a5a94a9d6b16c806ecaf8f2e8ac96f1
Erik <errikosmanolis@gmail.com>
2025-04-15 00:28:44 +0300

    Added diagnostics on hover and scroff

--- .config/nvim/lua/erik/diagnostics.lua
+++ .config/nvim/lua/erik/diagnostics.lua
@@ -5,3 +5,14 @@ vim.diagnostic.config({
   update_in_insert = false,  -- Disable in insert mode
   severity_sort = true,
 })
+
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+	group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+	callback = function ()
+		vim.diagnostic.open_float(nil, {focus=false})
+	end
+})
--- .config/nvim/lua/erik/set.lua
+++ .config/nvim/lua/erik/set.lua
@@ -17,3 +17,7 @@ vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
